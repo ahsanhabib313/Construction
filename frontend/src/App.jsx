4f4a84c96aa9417789512577cbc4a75653dff1c5
ahsanhabib313 <ahsanhabib313@gmail.com>
2025-08-04 12:16:04 +0600

    creating route for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import  React  from 'react'
 import './output.css'
 import Home from './components/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './components/About'
+import Services from './components/Services'
+import Projects from './components/Projects'
+import Blogs from './components/Blogs'
+import Contact from './components/Contact'
 
 function App() {
  
@@ -11,6 +16,11 @@ function App() {
        {  <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>} />
+                <Route path='/services' element={<Services/>} />
+                <Route path='/projects' element={<Projects/>} />
+                <Route path='/blogs' element={<Blogs/>} />
+                <Route path='/contact' element={<Contact/>} />
             </Routes>
         </BrowserRouter> }
     </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-colors' : '#fe538d',
+        'secondary-brand': '#FFB703',
+        'text-color' : '#000'
+      },
+       fontFamily: {
+        // 'your-custom-name' will be the utility class (e.g., font-your-custom-name)
+        'rubik': ['Rubik', 'sans-serif'], // 'YourFontName' must match the font-family used in @import/@font-face
+      },
+      fontSize:{
+        
+        'h1':'4.5em',
+        'h2':'3em',
+        'h3':'2em',
+        'p':'1.2em',
+       
+      }
+    },
   },
   plugins: [],
 }
